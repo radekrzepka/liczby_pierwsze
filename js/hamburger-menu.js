400a71ada40eccebcc8dbb3bb150fd65070f10e1
Radosław Rzepka <49752436+radekrzepka@users.noreply.github.com>
2021-03-04 00:10:29 +0100

    fixing header responsiveness

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -1,9 +1,11 @@
 const hamburgerIcon = document.querySelector(".header--hamburger-icon");
 const naviagtionList = document.querySelector(".navigation--list");
 const navigation = document.querySelector(".navigation");
+const headerImage = document.querySelector(".navigation--logo");
 
 hamburgerIcon.addEventListener("click", () => {
     hamburgerIcon.classList.toggle("open");
     naviagtionList.classList.toggle("open");
     navigation.classList.toggle("open");
+    headerImage.classList.toggle("open");
 })
