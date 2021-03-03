06ea226857b66f24e2ccbb263e86a1794264e53b
Radosław Rzepka <49752436+radekrzepka@users.noreply.github.com>
2021-03-03 13:01:12 +0100

    adding arrows to slider and fixing slider

--- js/slider.js
+++ js/slider.js
@@ -3,11 +3,11 @@ const slideList = [{
         text: "Rozpoczynamy!"
     },
     {
-        img: "./images/slider/slider-img1.jpg",
+        img: "./images/slider/slider-img2.jpg",
         text: "Historia"
     },
     {
-        img: "./images/slider/slider-img1.jpg",
+        img: "./images/slider/slider-img3.jpg",
         text: "O mnie"
     }
 ];
@@ -15,10 +15,14 @@ const slideList = [{
 const header = document.querySelector(".header");
 const headerButton = document.querySelector(".header--button");
 const dots = [...document.querySelectorAll(".dot")];
+const arrows = document.querySelectorAll(".header--arrow");
 
 let active = 0;
 
-const changeDot = () => {
+const changeContent = () => {
+    header.style.backgroundImage = `url(${slideList[active].img})`;
+    headerButton.textContent = slideList[active].text;
+
     const activeDot = dots.findIndex(dot => dot.classList.contains("active"));
     dots[activeDot].classList.remove("active");
     dots[active].classList.add("active");
@@ -27,10 +31,19 @@ const changeDot = () => {
 dots.forEach((dot, index) => {
     dot.addEventListener("click", () => {
         active = index;
+        changeContent();
+    })
+})
 
-        header.style.backgroundImage = `url: ("${slideList[active].img}")`;
-        headerButton.textContent = slideList[active].text;
-
-        changeDot();
+arrows.forEach((arrow, index) => {
+    arrow.addEventListener("click", () => {
+        if (index == 1) {
+            active++;
+            if (active == slideList.length) active = 0;
+        } else {
+            active--;
+            if (active < 0) active = slideList.length - 1;
+        }
+        changeContent();
     })
 })
