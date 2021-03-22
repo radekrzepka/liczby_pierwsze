b60d960d288b31b3fe439831b25b59711ab794fa
Radosław Rzepka <49752436+radekrzepka@users.noreply.github.com>
2021-03-22 15:37:34 +0100

    added eratostenes sieve

--- js/slider.js
+++ js/slider.js
@@ -37,7 +37,7 @@ const slider = () => {
 
     const changeSlide = () => {
         active++;
-        if(active === slideList.length) active = 0;
+        if (active === slideList.length) active = 0;
 
         changeContent();
     }
